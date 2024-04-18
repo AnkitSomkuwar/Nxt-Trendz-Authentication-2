68c010fe38caf2df8784e4e3c067d1581aae07e7
ankit <somkankit998.com>
2024-04-18 08:23:52 +0000

    comm

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/cart" component={Cart} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
